ac6b97a06b53d00cae63107f8d4236b31d69a073
abbas ali <raja,abbas614786@gmail.com>
2026-07-10 02:42:21 -0700

    Day 6: Python Basics

---
+++ Day-06/module-3.py
@@ -0,0 +1,9 @@
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+favorite_ai = input("Enter your favorite AI tool: ")
+
+print("\n----- Profile -----")
+print(f"Hello {name}!")
+print(f"You are {age} years old.")
+print(f"Your favorite AI tool is {favorite_ai}.")
+print("Welcome to the AI Engineer Journey!")
